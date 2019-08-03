b51814b1078dfc3c3b5098f19f536db5511c9309
rahul.jain <rahul.jain@gdn-commerce.com>
2019-08-03 17:59:05 +0530

    foundation

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div class="nav-bar">
+      <b-row class="nav-border">
+        <img id="logo-image" src="https://img.icons8.com/color/48/000000/wolfram-alpha.png">
+        <h2 class="logo">Dhundho</h2>
+        <a href="#" id="about-link" target="_blank">About</a>
+        <a href="#" id="help-link" target="_blank">Help</a>
+      </b-row>
+      
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#about-link {
+    color: #333333;
+    position: relative;
+    left: 74%;
+    font-size: 18px;
+}
+#help-link {
+    color: #333333;
+    position: relative;
+    left: 75%;
+    font-size: 18px;
+}
+.nav-bar {
+    margin: 0% 6% 1% 6%;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+}
+#logo-image {
+    margin-top: 0%;
+    position: relative;
+    top: -4px;
+}
+.nav-border {
+  padding-top: 1%;
+  background: #f7f7f7;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home-container">
+    <!-- <HelloWorld msg="Kuch bhi dhundho"/> -->
+    <b-row>
+      <b-col>
+        <b-form-input
+          id="search-input"
+          type="text"
+          required
+          placeholder="Search product"
+        ></b-form-input>
+      </b-col>
+      <b-col>
+        <b-button variant="success" id="search-button">Search</b-button>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+// export default {
+//   name: 'home',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style lang="scss">
+  #search-input {
+    width: 122%;
+    margin-left: 20%;
+    height: 50px;
+    font-size: 20px;
+    background: url(https://img.icons8.com/color/48/000000/search.png) no-repeat;
+    background-position: 98%;
+    background-size: 4%;
+  }
+  .home-container {
+    margin: 2% 10%;
+  }
+  #search-button {
+    margin-left: 2%;
+    width: 30%;
+    height: 50px;
+    font-size: 20px;
+  }
+</style>
